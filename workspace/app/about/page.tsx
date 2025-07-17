7d40d3e278bec04b36485d9809f45d71d1433f40
Josh Varkey <28jvarkey@gmail.com>
2025-07-16 20:30:30 -0400

    created the betting page where users can select bets

---
+++ workspace/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-rose-500 mb-4">Wedding Bets</h1>
+      <p className="text-gray-700">Here you’ll find all the fun bets for the big day 🎉</p>
+      {/* You can map over bet data here later */}
+    </main>
+  );
+}
